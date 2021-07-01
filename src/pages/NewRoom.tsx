9ce072cf6efefc459dd35d258515440261558f3d
wellysonvie <wellysonvie@gmail.com>
2021-07-01 00:15:59 -0300

    feat: create room

--- src/pages/NewRoom.tsx
+++ src/pages/NewRoom.tsx
@@ -1,14 +1,34 @@
-import { Link } from "react-router-dom";
+import { FormEvent, useState } from "react";
+import { Link, useHistory } from "react-router-dom";
 
 import illustrationImg from "../assets/images/illustration.svg";
 import logoImg from "../assets/images/logo.svg";
 import Button from "../components/Button";
 import { useAuth } from "../hooks/useAuth";
+import { database } from "../services/firebase";
 
 import "../styles/auth.scss";
 
 const NewRoom = () => {
-  //const { user } = useAuth();
+  const { user } = useAuth();
+  const history = useHistory();
+  const [newRoom, setNewRoom] = useState("");
+
+  async function handleCreateRoom(event: FormEvent) {
+    event.preventDefault();
+
+    if (newRoom.trim() === "") {
+      return;
+    }
+
+    const roomRef = database.ref("rooms");
+    const firebaseRoom = await roomRef.push({
+      title: newRoom.trim(),
+      authorId: user?.id,
+    });
+
+    history.push(`/rooms/${firebaseRoom.key}`);
+  }
 
   return (
     <div id="page-auth">
@@ -24,8 +44,13 @@ const NewRoom = () => {
         <div className="main-content">
           <img src={logoImg} alt="Letmeask" />
           <h2>Criar uma nova sala</h2>
-          <form>
-            <input type="text" placeholder="Nome da sala" />
+          <form onSubmit={handleCreateRoom}>
+            <input
+              type="text"
+              placeholder="Nome da sala"
+              onChange={({ target }) => setNewRoom(target.value)}
+              value={newRoom}
+            />
             <Button type="submit">Criar sala</Button>
           </form>
           <p>
---
+++ src/pages/Room.tsx
@@ -0,0 +1,18 @@
+import logoImg from "../assets/images/logo.svg";
+
+const Room = () => {
+  return (
+    <div id="page-room">
+      <header>
+        <div className="content">
+          <img src={logoImg} alt="Letmeask" />
+          <div>código</div>
+        </div>
+      </header>
+
+      <main></main>
+    </div>
+  );
+};
+
+export default Room;
